fb35ad71a4d196564fd4309b2e8e6d44111e20d5
Your New Name <mohammednizar078@gmail.com>
2026-09-03 21:51:49 +0530

    Add Netlify redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App";
 import Admin from "./admin";
+import "./index.css";
+
+const isAdminPage = window.location.pathname === "/admin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <React.StrictMode>{isAdminPage ? <Admin /> : <App />}</React.StrictMode>,
 );
